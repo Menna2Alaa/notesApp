97a65a58d4fe9bc8b26e6d611721f6beb6e755f8
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-06-13 00:44:13 +0300

    create cutome text field and constants file

--- lib/views/widgets/add_note_button.dart
+++ lib/views/widgets/add_note_button.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/cutome_text_field.dart';
 
 class AddNoteButton extends StatelessWidget {
   const AddNoteButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(children: [SizedBox(height: 32), CutomeTextField()]),
+    );
   }
 }
