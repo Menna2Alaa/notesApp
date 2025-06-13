dce3888a4b88c078602e55160095f41c67a924e1
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-06-13 23:01:18 +0300

    add custome app bar to edit note view body

--- lib/views/widgets/custome_app_bar.dart
+++ lib/views/widgets/custome_app_bar.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custome_search_icon.dart';
 
 class CustomeAppBar extends StatelessWidget {
-  const CustomeAppBar({super.key});
+  const CustomeAppBar({super.key, required this.text, required this.icon});
+  final String text;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [
-        Text("Notes", style: TextStyle(fontSize: 32)),
+      children: [
+        Text(text, style: const TextStyle(fontSize: 32)),
         Spacer(),
-        CustomeSearchIcon(),
+        CustomeSearchIcon(icon: icon),
       ],
     );
   }
