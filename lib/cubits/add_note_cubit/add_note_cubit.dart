0abd8106ca7f86f02e70d8462b1f1af6c6b71a0d
Menna <120278348+Menna2Alaa@users.noreply.github.com>
2025-06-20 01:34:08 +0300

    small change in add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
@@ -9,10 +9,11 @@ part 'add_notes_state.dart';
 class AddNoteCubit extends Cubit<AddNotesState> {
   AddNoteCubit() : super(AddNotesInitial());
 
-  addNote(NoteModel note) async {
+  Future<void> addNote(NoteModel note) async {
     emit(AddNotesLoading());
+
     try {
-      var notesBox = Hive.box(kNotesBox);
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
       await notesBox.add(note);
       emit(AddNotesSuccess());
     } catch (e) {
